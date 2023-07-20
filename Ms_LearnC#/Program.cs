f7e4075f9897c0c735df55f26c0e94ed91ecd85d
Annemarie <126640310+AceAnnemarie@users.noreply.github.com>
2023-07-20 22:09:41 +1000

    Add files via upload

---
+++ Ms_LearnC#/Program.cs
@@ -0,0 +1,97 @@
+﻿/* See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");*/
+
+/* Simple calucation
+int a = 18;
+int b = 6;
+int c = a + b;
+Console.WriteLine(c);
+*/
+
+/*
+int a = 18;
+int b = 6;
+int c = a + b;
+*/
+
+/*
+// Substraction 
+
+c = a - b;
+Console.WriteLine(c);
+
+//Multiplication
+
+c = a * b;
+Console.WriteLine(c);
+
+//divison
+
+c = a / b;
+Console.WriteLine(c);
+*/
+
+/*
+// includes a statement of the operation and the result it gives
+
+c = a - b;
+Console.WriteLine("substraction "  + c ) ;
+
+//includes a statement of the operation and result it gives
+
+c = a * b;
+Console.WriteLine($"times = {c} ");
+
+//Create a sentence that includes the operation and its result (neatly stated)
+
+c= a / b ;
+Console.WriteLine($"{a} divided by {b} = {c}");
+
+*/
+
+/*
+void workwithIntergers ()
+
+{
+    int a = 18;
+    int b = 6;
+    int c = a + b;
+
+
+    c = a - b;
+    Console.WriteLine("substraction "  + c ) ;
+
+    c = a * b;
+    Console.WriteLine($"multiplication = {c} ");
+
+    c = a / b ;
+    Console.WriteLine($"{a} divided by {b} = {c}");
+
+}
+
+ workwithIntergers ();// Calling the function above to calulate
+*/
+
+/*
+
+void workwithIntergers ()
+
+{
+    int a = 18;
+    int b = 6;
+    int c = a + b;
+
+
+    c = a - b;
+    Console.WriteLine($"{a} substracted by {b} = {c}" ) ;
+
+    c = a * b;
+    Console.WriteLine($" {a} multiplied by {b} = {c} ");
+
+    c = a / b ;
+    Console.WriteLine($"{a} divided by {b} = {c}");
+
+}
+
+ workwithIntergers ();// Calling the function above to calulate
+ /*
