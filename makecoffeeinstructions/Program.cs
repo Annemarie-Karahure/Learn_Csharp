e9786d0533c9b3c4f76169596cc8d8f2dfcb4b82
Annemarie <126640310+AceAnnemarie@users.noreply.github.com>
2023-07-23 18:58:48 +1000

    Add files via upload

---
+++ makecoffeeinstructions/Program.cs
@@ -0,0 +1,41 @@
+﻿
+    using System;
+    class CoffeeMaker {
+    static void Main (string[] args) {
+
+    //step 1: Boil Water
+
+    Console.WriteLine ("1. Boil Water: Fill the kettle with fresh cold water.Place the pot on the stove and heat it until the water comes to a boil.");
+
+    //step 2: Grind coffee beans 
+
+    Console.WriteLine ("2.Grind coffee beans to a medium-fine grind");
+
+    //Step 3: Add coffee to filter
+
+    Console.WriteLine ("Add coffee to filter:Place the coffee filter in the coffee marker and add the ground coffee to the filter.");
+
+    //Step 4: Add water to coffee marker
+
+    Console.WriteLine("4.Pour the hot water into the coffee maker.");
+
+    //Step 5:Brew coffee
+
+    Console.WriteLine("5.Brew coffee: Turn on the coffee marker and let it brew the coffee for a few minutes.");
+
+
+    //Step 6: Pour coffee into mug
+    Console.WriteLine ("6.Once the coffee is brewed, pour it into a mug");
+
+
+    //Step 7: Add milk and Sugar 
+    Console.WriteLine("7.Add milk and sugar (optional): If you prefer your coffee with milk and sugar, you can add them at this point.");
+
+
+    //Step 8: Enjoy your coffee
+    Console.WriteLine("8. Enjoy your coffee: Sit back,relax, and enjoy your freshly brewed cup of coffee!");
+
+
+    }
+
+ }
